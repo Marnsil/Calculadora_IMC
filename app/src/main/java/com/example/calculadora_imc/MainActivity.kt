ad9b72ce75cc796a6abfeba1bcad9f2abad5513f
Marcos <marcos_zoo@hotmail.com>
2021-03-14 18:54:02 -0300

    Criado calculadora IMC.

---
+++ app/src/main/java/com/example/calculadora_imc/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.calculadora_imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+
+    }
+    private fun setListeners() {
+        calcularBNT?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = (peso / (altura * altura))
+            calcularIMC.text = "SEU IMC É:  %.2f".format(imc)
+
+        }
+    }
+}
